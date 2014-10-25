ee3cbb157e7c9bd9e2a599840ccb8d129d42aaf9
yassirh <yassir.hannoun@gmail.com>
2014-10-25 14:14:20 +0200

    Added the missing toolbar on the settings activity

--- DigitalOceanSwimmer/src/main/java/com/yassirh/digitalocean/ui/SettingsActivity.java
+++ DigitalOceanSwimmer/src/main/java/com/yassirh/digitalocean/ui/SettingsActivity.java
@@ -3,17 +3,26 @@ package com.yassirh.digitalocean.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import com.yassirh.digitalocean.R;
 
 public class SettingsActivity extends ActionBarActivity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
         PrefsFragment prefsFragment = new PrefsFragment();
         prefsFragment.setActivity(this);
 		getSupportFragmentManager().beginTransaction()
-        	.replace(android.R.id.content, prefsFragment).commit();
+        	.replace(R.id.content_frame, prefsFragment).commit();
 	}	
 	
 	@Override
