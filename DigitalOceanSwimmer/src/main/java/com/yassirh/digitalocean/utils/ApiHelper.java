1c9f87abde4c291742e1abafef2fcf06aeb6526e
Yassir HANNOUN <yassir.hannoun@gmail.com>
2015-09-29 01:43:05 +0200

    Added the flag for Toronto region

--- DigitalOceanSwimmer/src/main/java/com/yassirh/digitalocean/utils/ApiHelper.java
+++ DigitalOceanSwimmer/src/main/java/com/yassirh/digitalocean/utils/ApiHelper.java
@@ -164,6 +164,8 @@ public class ApiHelper {
         	return isAvailable ? R.drawable.uk_flag : R.drawable.uk_flag_unavailable;
 		else if(region.contains("Frankfurt"))
 			return isAvailable ? R.drawable.de_flag : R.drawable.de_flag_unavailable;
+		else if(region.contains("Toronto"))
+			return isAvailable ? R.drawable.ca_flag : R.drawable.ca_flag_unavailable;
 		else
 			return isAvailable ? R.drawable.unknown : R.drawable.unknown_unavailable;
 	}
